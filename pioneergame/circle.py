890ffa9f2285056eec17a421aef3361896ffaf7c
chebur5581 <110908198+chebur5581@users.noreply.github.com>
2025-04-07 14:23:36 +0300

    Add files via upload

--- pioneergame/circle.py
+++ pioneergame/circle.py
@@ -4,14 +4,15 @@ from .window import Window
 
 class Circle(pg.Rect):  # just to have some benefits, like colliderect method
     def __init__(self, window: Window, x, y, radius,
-                 color: str | pg.Color | tuple[int, int, int] = (255, 0, 255)):
-        super().__init__(x, y, radius * 2, radius * 2)
+                 color: str | pg.Color | tuple[int, int, int] = (255, 0, 255), thickness: int = 0):
+        super().__init__(x - radius, y - radius, radius * 2, radius * 2)
 
         self.window = window
         self.color = color
+        self.thickness = thickness
 
     def draw(self) -> None:
-        pg.draw.circle(self.window.screen, self.color, self.center, self.width / 2)
+        pg.draw.circle(self.window.screen, self.color, self.center, self.width / 2, self.thickness)
 
     def draw_box(self) -> None:
         pg.draw.rect(self.window.screen, self.color, self)
